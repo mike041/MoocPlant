3db3bf4b30bc6f02a0a0b0e9219d22a341886b6e
ytxu <609037724@qq.com>
2023-02-24 18:11:58 +0800

    第5次次

--- Imoocmapi/views/user_view.py
+++ Imoocmapi/views/user_view.py
@@ -1,37 +1,43 @@
 # coding=utf-8
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
 from ..models import UserInfo
 from django.http import HttpResponseRedirect, HttpResponse
 import json
 import jwt
+from ..utils.common import handle_redis
 
 
 def check_login(func):
     def wrapper(request, *args, **kwargs):
-        token = request.COOKIES.get("token",None)
-        if token == 'null':
+        token = request.COOKIES.get("token", None)
+        if token == "null":
             return HttpResponseRedirect("/login/")
         else:
             try:
-                jwt.decode(token, "sercet", algorithms=['HS256'])
-                return func(request, *args, **kwargs)
+                user_data = jwt.decode(token, "sercet", algorithms=['HS256'])
+                user_key = user_data.get("username")
+                if handle_redis.get_value_str("token_" + user_key) is not None:
+                    return func(request, *args, **kwargs)
+                else:
+                    return HttpResponseRedirect("/login/")
+
             except jwt.InvalidTokenError:
                 return HttpResponseRedirect("/login/")
 
     return wrapper
 
 
-
 def login(request):
+    data = {
+        "code": 10000,
+        "msg": "登录成功",
+        "url": "/index/",
+        "token": "",
+        "nick_name": ""
+    }
     if request.method == 'POST':
-        data = {
-            "code": 10000,
-            "msg": "登录成功",
-            "url": "/index/",
-            "token": "",
-            "nick_name":""
-        }
+
         request_data = json.loads(request.body.decode("utf-8"))
         username = request_data.get("username")
         password = request_data.get("password")
@@ -40,19 +46,20 @@ def login(request):
             token = jwt.encode(request_data, "sercet")
             data["token"] = token
             data["nick_name"] = nick_name
+            handle_redis.set_value("token_" + username, token)
             return HttpResponse(json.dumps(data))
         else:
             return render(request, "login.html")
     else:
         token = request.COOKIES.get("token", None)
-        if token == 'null':
+        if token == "null":
             return render(request, "login.html")
         else:
             try:
                 jwt.decode(token, "sercet", algorithms=['HS256'])
-                return HttpResponseRedirect("/")
+                return HttpResponse(json.dumps(data))
             except jwt.InvalidTokenError:
-                return HttpResponseRedirect("/login/")
+                return render(request, "login.html")
         return render(request, "login.html")
 
 
