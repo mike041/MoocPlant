002aded67e4ab2dcb8695d90b4df7a78f066aae7
ytxu <609037724@qq.com>
2023-04-12 15:43:42 +0800

    增加bug图片展示位置

--- static/assets/js/commons.js
+++ static/assets/js/commons.js
@@ -716,8 +716,8 @@ function BigBig(src, width, height) {
         var modal = $(this);
         modal.find('.modal-dialog').css({'margin-left': (document.body.clientWidth - width * 1) / 6 + 'px'})
         modal.find('.modal-body #image').attr("src", src)
-            .attr("width", width * 25)
-            .attr("height", height * 25);
+            .attr("width", width * 23)
+            .attr("height", height * 23);
     });
 }
 
